5283ceca3717bcf3296e52c8853e1a505b265ca9
Vlasis Mentz <vlasism@gmail.com>
2022-04-07 22:59:17 +0300

    fix dependencies

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world, this is my first React Native App</Text>
-      <StatusBar style='auto' />
+      <Text>Hello world</Text>
     </View>
   );
 }
