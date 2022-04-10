f978b1c3053496246e64aaae14607dfeef96b582
Vlasis Mentz <vlasism@gmail.com>
2022-04-10 11:59:55 +0300

    add components folder and components

--- App.js
+++ App.js
@@ -14,6 +14,16 @@ const theme = {
 };
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,15 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { Text, View, FlatList, SafeArea } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NFTCard, FocusedStatusBar, HomeHeader } from '../components/';
+import { COLORS, NFTData } from '../constants';
 
 function Home() {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
+  );
 }
 
 export default Home;
